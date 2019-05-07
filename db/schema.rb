bcaac584659b14b3928ed97be4500acc3933100b
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-05-07 13:01:57 +0200

    Generate and run Comment migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_07_081012) do
+ActiveRecord::Schema.define(version: 2019_05_07_105527) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.string "commentable_type"
+    t.integer "commentable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
