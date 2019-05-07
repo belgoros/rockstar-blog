a68590d44413772536e9f54d776888789da38f4e
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-05-07 10:37:51 +0200

    Add post validations and model spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should be valid' do
+    post = build(:post)
+    expect(post).to be_valid
+  end
+
+  it 'is invalid without title' do
+    post = build(:post, title: nil)
+    expect(post).not_to be_valid
+  end
+
+  it 'is invalid without body' do
+    post = build(:post, body: nil)
+    expect(post).not_to be_valid
+  end
+
+  it 'should have unique title' do
+    existing_post = create(:post)
+    new_post = build(:post, title: existing_post.title)
+    expect(new_post).not_to be_valid
+  end
 end
